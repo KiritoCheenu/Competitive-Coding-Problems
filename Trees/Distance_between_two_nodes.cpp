7d22dad75ff7ff5547de704a5e2145c971d60e13
KiritoCheenu <61424879+srivastavachinmay@users.noreply.github.com>
2021-06-29 01:54:38 +0530

    Create Distance_between_two_nodes.cpp

---
+++ Trees/Distance_between_two_nodes.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data) : data(data)
+    {
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node *LCA(Node *root, int n1, int n2)
+{
+    if (root == NULL)
+        return NULL;
+    if (root->data == n1 || root->data == n2)
+        return root;
+    Node *left = LCA(root->left, n1, n2);
+    Node *right = LCA(root->right, n1, n2);
+    if (left != NULL && right != NULL)
+        return root;
+    if (left == NULL && right == NULL)
+        return root;
+    if (left != NULL && right == NULL)
+        return LCA(root->left, n1, n2);
+    if (left == NULL && right != NULL)
+        return LCA(root->right, n1, n2);
+}
+int findDist(Node *root, int k, int d)
+{
+    if (root == NULL)
+        return -1;
+    if (root->data == k)
+        return d;
+    int left = findDist(root->left, k, d + 1);
+    if (left != -1)
+        return left;
+    return findDist(root->right, k, d + 1);
+}
+int distBtwNodes(Node *root, int n1, int n2)
+{
+    Node *lca = LCA(root, n1, n2);
+    int d1 = findDist(lca, n1, 0);
+    int d2 = findDist(lca, n2, 0);
+    return d1 + d2;
+}
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    cout<<distBtwNodes(root,4,7);
+}
