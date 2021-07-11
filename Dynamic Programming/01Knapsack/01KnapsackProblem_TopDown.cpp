5d9cafbbaddfb968850eaec4db2187e0a21e6018
KiritoCheenu <61424879+srivastavachinmay@users.noreply.github.com>
2021-07-12 02:51:47 +0530

    Update 01KnapsackProblem_TopDown.cpp

--- Dynamic Programming/01Knapsack/01KnapsackProblem_TopDown.cpp
+++ Dynamic Programming/01Knapsack/01KnapsackProblem_TopDown.cpp
@@ -12,7 +12,7 @@ int knapsack(int wt[], int val[], int cap, int n)
         {
             if (wt[i - 1] <= j)
             {
-                dp[i][j] = max(val[i - 1] + dp[i - 1][cap - wt[i - 1]], dp[i - 1][j]);
+                dp[i][j] = max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
             }
             else
                 dp[i][j] = dp[i - 1][j];
